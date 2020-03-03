ae64e5d6dd797e57b69ac94151345a3f8d372464
saifur703 <saifur703@gmail.com>
2020-03-04 03:50:57 +0600

    Truthy Value and False Value

---
+++ truthy-falsy.js
@@ -0,0 +1,27 @@
+/**
+ * False Value
+ *
+ * 0
+ * "" -> empty string
+ * undefined
+ * null
+ * NaN
+ */
+
+let x = {};
+if (x) {
+  console.log('Condition is True');
+} else {
+  console.log('Condition is False');
+}
+
+/**
+ * Truthy Value
+ *
+ * 123
+ * -45
+ * "0"
+ * " "
+ *  []
+ * {}
+ */
