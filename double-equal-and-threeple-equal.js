a7a028f906344f5bbc6581b44d7a95fea30dbc5b
saifur703 <saifur703@gmail.com>
2020-03-04 21:58:09 +0600

    practice between == and ===

---
+++ double-equal-and-threeple-equal.js
@@ -0,0 +1,46 @@
+// ==
+// let x = '5';
+// let y = 5;
+// if (x == y) {
+//   console.log(`${x} and ${y} is equal.`);
+// } else {
+//   console.log(`${x} and ${y} is not equal.`);
+// }
+
+// // ===
+// if (x === y) {
+//   console.log(`${x} and ${y} is equal.`);
+// } else {
+//   console.log(`${x} and ${y} Datatype is not equal.`);
+// }
+
+// // ==
+// let a = 0;
+// let b = false;
+// if (a == b) {
+//   console.log('False');
+// } else {
+//   console.log('True');
+// }
+// // ===
+// if (a === b) {
+//   console.log('False');
+// } else {
+//   console.log('Datatype is not equal and both are false');
+// }
+
+// if ('2' === 2) {
+//   console.log('Inside if');
+// } else {
+//   console.log('Inside else');
+// }
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+console.log('\n');
+
+let arr = [4, 55, 10, 'Rahim', 98];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
