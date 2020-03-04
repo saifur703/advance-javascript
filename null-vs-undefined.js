98664c42a6a775fac15acd409a9628e3a3b1e5c8
saifur703 <saifur703@gmail.com>
2020-03-04 10:52:38 +0600

    Null VS Undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,23 @@
+// Null
+let var1;
+console.log(var1);
+
+// Undefined
+function add(a, b) {
+  console.log(a + b); // It will be return
+}
+const addition = add(5, 10);
+console.log(addition);
+
+// Undefined
+let obj = {
+  name: 'Saifur',
+  age: 24
+};
+console.log(obj.gf); // gf is undefined
+
+function doSomething(x, y) {
+  console.log(y);
+}
+
+doSomething(32);
